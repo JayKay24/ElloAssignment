3d584f2e8f2a4487a49fb354f6765d7a32304da1
Jimmy <jameskinyua590@gmail.com>
2022-04-12 10:55:25 +0300

    Install & configure apollo-client

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import './App.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+import StyledContainer from './components/styled/StyledContainer';
+import StyledHeader from './components/styled/StyledHeader';
+import StyledH1 from './components/styled/StyledH1';
+import StyledMain from './components/styled/StyledMain';
+
+const client = new ApolloClient({
+  uri: 'https://fullstack-engineer-test-n4ouilzfna-uc.a.run.app/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <ApolloProvider client={client}>
+      <StyledContainer>
+        <StyledHeader>
+          <StyledH1>Ello Challenge</StyledH1>
+        </StyledHeader>
+        <StyledMain>Main</StyledMain>
+      </StyledContainer>
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ src/components/styled/StyledHeader.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  height: 5vh;
+  padding: 10px 5px;
+`;
+
+export default StyledHeader;
