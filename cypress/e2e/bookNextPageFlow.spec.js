e583457a461e0d0983942eaab50b0da19773f92e
Jimmy <jameskinyua590@gmail.com>
2022-04-15 18:25:50 +0300

    Mock server response from graphql endpoint

--- cypress/e2e/bookNextPageFlow.spec.js
+++ cypress/e2e/bookNextPageFlow.spec.js
@@ -2,19 +2,22 @@
 /* eslint-disable no-undef */
 import mockedBook from '../../src/components/utils/mockedBook';
 
-// TODO: Figure out how to implement GQL endpoint mocking
-// with Cypress
+beforeEach(() => {
+  cy.intercept('POST', 'https://fullstack-engineer-test-n4ouilzfna-uc.a.run.app/graphql', {
+    statusCode: 200,
+    body: { data: { book: { ...mockedBook } } },
+  });
+  cy.visit('/');
+});
 
 describe('Book Flow', () => {
   it('allows a user to load the application and go to the next book page', () => {
-    cy.visit('/');
     cy.findByText(/loading.../i).should('be.visible');
     cy.findByRole('button', { name: /next/i }).click();
     cy.contains('elephants').should('be.visible');
   });
 
   it('allows a user to click a word and go to the relevant token view', () => {
-    cy.visit('/');
     cy.findByText(/loading.../i).should('be.visible');
     cy.findByText(mockedBook.author).should('be.visible');
     cy.findByRole('link', { name: /parrots/i }).click();
